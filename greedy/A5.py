e04aa849a314aedca07a969dfc110217043392f7
Park ChangHyub <thwlckd@naver.com>
2023-03-23 13:30:38 +0900

    2019 SW 마에스트로 기출

--- greedy/A5.py
+++ greedy/A5.py
@@ -1,32 +1,32 @@
 # 그리디
 # 볼링공 고르기
 
-# n, m = map(int, input().split())
-# weight = list(map(int, input().split()))
-
-# count = 0
-# memo = [[False] * n for _ in range(n)]
-# for i in range(n):
-#     for j in range(i+1, n):
-#         if weight[i] != weight[j] and memo[i][j] == False:
-#             memo[i][j] = True
-#             count += 1
-
-# print(count)
-
-# 무게별 경우의 수 = A가 고른 공과 무게가 같은 공의 개수 * B가 선택 가능한 경우의 수
 n, m = map(int, input().split())
 weight = list(map(int, input().split()))
 
-array = [0] * 11  # 1~10 무게를 담을 수 있는 리스트
-for x in weight:
-    array[x] += 1  # 무게별 볼링공 카운트
-
 count = 0
-for i in range(1, m+1):
-    n -= array[i]  # B 선택 경우의 수
-    count += array[i] * n
+memo = [[False] * n for _ in range(n)]
+for i in range(n):
+    for j in range(i+1, n):
+        if weight[i] != weight[j] and memo[i][j] == False:
+            memo[i][j] = True
+            count += 1
+
 print(count)
+
+# # 무게별 경우의 수 = A가 고른 공과 무게가 같은 공의 개수 * B가 선택 가능한 경우의 수
+# n, m = map(int, input().split())
+# weight = list(map(int, input().split()))
+
+# array = [0] * 11  # 1~10 무게를 담을 수 있는 리스트
+# for x in weight:
+#     array[x] += 1  # 무게별 볼링공 카운트
+
+# count = 0
+# for i in range(1, m+1):
+#     n -= array[i]  # B 선택 경우의 수
+#     count += array[i] * n
+# print(count)
 
 '''
 입력 예시 1
